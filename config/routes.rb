aa737bb4a8d6d76d880b75eb4dc36c0b33db697f
Paul Daniel <paulgdan@gmail.com>
2017-07-09 00:19:07 -0700

    refresh

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :remark, controller: 'remark'
+  resource :refresh, controller: 'refresh'
 end
