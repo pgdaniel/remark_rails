8a892e5df7ddc3c0e2e181445a7c24259ee9b9d3
Paul Daniel <paulgdan@gmail.com>
2017-07-09 18:20:34 -0700

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'gallery/index'
-
-  get 'gallery/new'
-
-  get 'gallery/create'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :slides
   resource :refresh, controller: 'refresh'
+
+  root to: 'slides#index'
 end
