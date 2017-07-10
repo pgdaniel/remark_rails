7278f6b6dacf2fd258d30d688cd9d31c11119456
Paul Daniel <paulgdan@gmail.com>
2017-07-09 18:57:11 -0700

    update uploader to use fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,6 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   else
-    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
